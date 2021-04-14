3ea06094f5e385cbd901955afcee66caffbee7eb
Aviad <aviad.bagno@gmail.com>
2021-04-14 14:16:04 +0300

    Added a basic checking for read_set on myset.c

--- myset/myset.c
+++ myset/myset.c
@@ -2,4 +2,18 @@
  * 
  * חשוב!!! לבדוק בספר אם קיים המודול שמאפשר להגדיר גודל למשתנה
  * 
- * /
+ */
+
+#include <stdio.h>
+#include "set.h"
+
+int main()
+{
+    set_member arr[] = {127, 0, 34, -1};
+    set s;
+    empty_set(&s);
+    read_set(&s, arr);
+    printf("%u\n", s);
+
+    return 0;
+}
